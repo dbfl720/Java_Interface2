e9cb05441c2b923f6cc981c75508899aee4d5cdd
홍유리 <116433637+dbfl720@users.noreply.github.com>
2022-12-18 20:21:40 +0900

    Java_Interface
    Java_Interface 혼자 공부하는 자바2

---
+++ Driver/src/main/java/com/example/driver/Driver.java
@@ -0,0 +1,11 @@
+package com.example.driver;
+
+public class Driver {
+    public void drive (Vehicle vehicle) {
+        if(vehicle instanceof Bus){
+            Bus bus = (Bus) vehicle;
+            bus.checkFare();
+        }
+        vehicle.run();
+    }
+}
